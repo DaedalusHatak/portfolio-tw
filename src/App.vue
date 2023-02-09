1339d9d22158d24c666281f4ef7d1974e3b3af33
DaedalusHatak <pawelkic55@gmail.com>
2023-02-09 22:19:40 +0100

    next section

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <script>
-import HeroSection from './components/HeroSection.vue'
-import AboutMe from './components/AboutMe.vue'
-import SectionTemplate from './components/SectionTemplate.vue'
+import HeroSection from './components/HeroSection.vue';
+import AboutMe from './components/AboutMe.vue';
+import MyServices from './components/MyServices.vue';
+import SectionTemplate from './components/SectionTemplate.vue';
 export default{
-components:{HeroSection,AboutMe,SectionTemplate},
+components:{HeroSection,AboutMe,SectionTemplate,MyServices},
 };
 </script>
 
 <template>
  <section-template class=" bg-zinc-800"><hero-section class=""></hero-section></section-template>
- <!-- <section-template class="bg-dark-1200 h-96"> <about-me></about-me></section-template> -->
+ <section-template class="bg-dark-1200"> <about-me></about-me></section-template>
+ <section-template class="bg-dark-1100"> <my-services></my-services></section-template>
 </template>
 
 <style scoped>
---
+++ src/components/MyServices.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="py-12 lg:grid  justify-center text-center">
+    <div class="text-5xl font-semibold pb-12 mb-12">What can I do best?</div>
+    <div class="lg:grid text-center justify-self-center grid-cols-3 gap-12 lg:w-4/6">
+      <div class="p-5">
+        <p class="text-xl leading-12 font-semibold">Webflow Development</p>
+        <p class="text-gray-1300 leading-7">
+          Really boy law county she unable her sister. Feet you off its like
+          like six. Among sex are leave law built now. In built table in an
+          rapid blush. Merits behind on afraid or warmly.
+        </p>
+      </div>
+      <div class="bg-dark-1200 p-5" >
+       <p class="  text-xl leading-12 font-semibold">Figma Web Designing</p>
+
+        <p class="text-gray-1300 leading-7">
+          Really boy law county she unable her sister. Feet you off its like
+          like six. Among sex are leave law built now. In built table in an
+          rapid blush. Merits behind on afraid or warmly.
+        </p>
+      </div>
+      <div class="p-5">
+        <p class="text-xl leading-12 font-semibold">Shopify Development</p>
+
+        <p class="text-gray-1300 leading-7">
+          Really boy law county she unable her sister. Feet you off its like
+          like six. Among sex are leave law built now. In built table in an
+          rapid blush. Merits behind on afraid or warmly.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
