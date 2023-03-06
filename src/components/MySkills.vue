b41187ba658c961e38146ab27805bd13aaeeb378
DaedalusHatak <pawelkic55@gmail.com>
2023-03-06 15:56:23 +0100

    update

--- src/components/MySkills.vue
+++ src/components/MySkills.vue
@@ -8,8 +8,10 @@
 
 			<div class="font-bold text-4xl py-7">My Skills & Tools</div>
 			<p class="text-gray-1100"
-				>I work with Visual Studio, and I'm going with Vue.js for my projects as it's good for smaller projects.
-				<br>For better quality and performance adding Tailwind/Vuetify and Vite is what I'm going for. <br> I'm also using Github to improve my websites safely.
+				>I work with Visual Studio, and I'm going with Vue.js for my projects as
+				it's good for smaller projects. <br />For better quality and performance
+				adding Tailwind/Vuetify and Vite is what I'm going for. <br />
+				I'm also using Github to improve my websites safely.
 			</p>
 		</div>
 		<div class="lg:col-span-3">
@@ -63,5 +65,4 @@
 li {
 	max-width: 650px;
 }
-
 </style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import ButtonGlow from './components/templates/ButtonGlow.vue'
+import MyModal from './components/templates/MyModal.vue';
 
 const app = createApp(App);
 app.component('ButtonGlow',ButtonGlow);
+app.component('MyModal',MyModal);
 
 
 app.mount('#app');
