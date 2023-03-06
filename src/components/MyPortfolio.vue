a72f54a6dd2cd87a675ec9470352094687368a31
DaedalusHatak <pawelkic55@gmail.com>
2023-03-06 15:44:33 +0100

    updated Portfolio with cards and modal prompt

--- src/components/MyPortfolio.vue
+++ src/components/MyPortfolio.vue
@@ -3,50 +3,61 @@
 		<h2 class="font-bold text-2xl text-orange-1100 text-with-line"
 			>Portfolio</h2
 		>
-		<h2 class="text-3xl md:text-5xl font-semibold pb-12 mb-12">My Creative Work</h2>
-		<div
-			class="py-12 grid lg:grid-cols-2  justify-items-center  text-center gap-16 md:gap-44"
+		<h2 class="text-3xl md:text-5xl font-semibold pb-12 mb-12"
+			>My Creative Work</h2
 		>
-		<div class="maxw ">	
-			<div class="border-2 border-orange-1100">
-				<img src="../assets/zsz1.png" alt="" />
-				
-			</div>
-			<div class="grid justify-items-center"><p class="pt-3 pb-3">Cięcie wodą 3D</p>
-				<a href="http://projects.pawel-kicinski.pl"><ButtonGlow  message="Go to project"></ButtonGlow></a></div>
-				
-			</div>
-
-			<div class="maxw ">	
-			<div class="border-2 border-orange-1100">
-				<img src="../assets/portfolio.png" alt="" />
-				
-			</div>
-			<div class="grid justify-items-center"><p class="pt-3 pb-3">Cięcie wodą 3D</p>
-				<a href="http://pawel-kicinski.pl"><ButtonGlow  message="Go to project"></ButtonGlow></a></div>
-				
-			</div>
-		</div></div
-	>
+		<div
+			class="py-12 grid lg:grid-cols-2 justify-items-center text-center gap-16 md:gap-44"
+		>
+			<div class="maxw" v-for="link in links">
+				<project-card
+					@currentLink="actLink"
+					:link="link.link"
+					@isVisible="isAvailable"
+					:title="link.title"
+					:image="link.img"
+				></project-card> </div></div
+	></div>
 </template>
 
 <style scoped>
-.maxw{
+.maxw {
 	max-width: 500px;
 }
-.button-link{
-	display:block;
-margin-top: 1em;
-padding:1em;
-width: 10em;
-
+.button-link {
+	display: block;
+	margin-top: 1em;
+	padding: 1em;
+	width: 10em;
 }
-img{
-	aspect-ratio: 3/2;
-	height: 100%;
-	width: 100%;
-}</style>
+
+</style>
 
 <script>
-export default {};
+import ProjectCard from './templates/ProjectCard.vue';
+export default {
+	components:{ProjectCard},
+	data() {
+		return {
+			isVisible: null,
+			actualLink:null,
+			links: [{link:'http://pawel-kicinski.pl',title:'My Portfolio', img:'portfolio.png'}, {link:'http://projects.pawel-kicinski.pl',title:'Cięcie wodą 3D', img:'zsz1.png'}],
+
+			
+		};
+	},
+
+	methods: {
+		isAvailable(data){
+			this.isVisible = data;
+			this.$emit('isVisible', this.isVisible);
+			this.$emit('href',this.actualLink)
+		},
+		actLink(data){
+			console.log(data);
+			this.actualLink=data;
+		},
+
+	},
+};
 </script>
