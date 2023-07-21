4de2c6a710dc51d65fc44bb55cbe9dccbcf650bb
DaedalusHatak <pawelkic55@gmail.com>
2023-07-21 17:35:53 +0200

    feat: latest version

--- src/send-email.php
+++ src/send-email.php
@@ -1,15 +1,21 @@
 <?php
 $name = $_POST['name'];
 $email = $_POST['email'];
-$message = $_POST['message'];
+$message = $_POST["message"];
 
-$to = 'pawelkic@serwer2339779.home.pl';
-$subject = 'New message from ' . $name;
-$body = 'Name: $name\nEmail: $email\n\n$message';
-$headers = 'From: $email';
+$to      = 'pawelkic@serwer2339779.home.pl';
+$subject = "New message from " . $name;
 
-if(mail($to,$subject,$body,$header)){
-    echo 'Email sent successfully'
-}
-else {echo 'Error:Something went wrong'}
+$headers = array(
+    'From' => "$email",
+    'Reply-To' => 'pawelkic@serwer2339779.home.pl',
+    'X-Mailer' => 'PHP/' . phpversion()
+);
+
+$email_message = "Name: $name\n";
+$email_message .= "Email: $email\n\n";
+$email_message .= "Message: \n$message\n";
+
+mail($to, $subject, $email_message, $headers);
+
 ?>
